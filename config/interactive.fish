f510e0f55875aca1a96aaddbe2e3bdae887aa48c
Edward J. Schwartz <edmcman@cmu.edu>
2024-05-09 15:33:00 -0400

    !$ and !!

--- config/interactive.fish
+++ config/interactive.fish
@@ -3,3 +3,28 @@
 #echo hello from interactive.fish
 
 alias bat=batcat
+
+# https://superuser.com/a/944589/130232
+function bind_bang
+    switch (commandline -t)[-1]
+        case "!"
+            commandline -t -- $history[1]
+            commandline -f repaint
+        case "*"
+            commandline -i !
+    end
+end
+
+function bind_dollar
+    switch (commandline -t)[-1]
+        case "!"
+            commandline -f backward-delete-char history-token-search-backward
+        case "*"
+            commandline -i '$'
+    end
+end
+
+function fish_user_key_bindings
+    bind ! bind_bang
+    bind '$' bind_dollar
+end
